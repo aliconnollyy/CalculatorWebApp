86f5908c524c7b61764d9455ba5f0fc1b93f2321
okaforc <okaforc@tcd.ie>
2022-11-03 12:12:29 +0000

    possible to separate string into values

--- calc_test.py
+++ calc_test.py
@@ -1,4 +1,4 @@
-from calculator import sub_calc
+from calculator import *
 
 def test_calculator():
     assert sub_calc(4, 6, "*") == 24
@@ -7,3 +7,24 @@ def test_calculator():
     assert sub_calc(4, 6, "-") == -2
     assert sub_calc(4, 6, "+") == 10
     assert sub_calc(4, 6, "^") == 4096
+    
+    ts_1 = "1+2"
+    ts_2 = "0*0"
+    ts_3 = "100-100-1001"
+    ts_4 = "3*3-1"
+    ts_5 = "0+4*0*180+1"
+    ts_6 = "8*2-4*4"
+    ts_7 = "-1-2*-3+-1000*0+3"
+    ts_8 = "3*3*3*3*3*3*-3*-3*-3"
+    ts_9 = "1/2/3"
+    ts_10 = "5^7^39384"
+    assert calc(ts_1) == ["1", "2"]
+    assert calc(ts_2) == ["0", "0"]
+    assert calc(ts_3) == ["100", "100", "1001"]
+    assert calc(ts_4) == ["3", "3", "1"]
+    assert calc(ts_5) == ["0", "4", "0", "180", "1"]
+    assert calc(ts_6) == ["8", "2", "4", "4"]
+    assert calc(ts_7) == ['-1', '2', '-3', '-1000', '0', '3']
+    assert calc(ts_8) == ['3', '3', '3', '3', '3', '3', '-3', '-3', '-3']
+    assert calc(ts_9) == ['1', '2', '3']
+    assert calc(ts_10) == ['5', '7', '39384']
--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+
 def sub_calc(a, b, op):
     match op:
         case "+":
@@ -11,4 +12,47 @@ def sub_calc(a, b, op):
         case "^":
             return a**b
         case _:
-            return None 
+            return None
+
+
+def calc(string: str):
+    # return eval("1 * - 2")
+    return sep(string)
+
+
+def sep(eq: str):
+    nums = []
+    ops = []
+    neg_pos = []  # positions to negate characters
+    ops_in_a_row = 0
+    for i in range(len(eq)):
+        c = eq[i]
+        if not c.isalnum():
+            # if the current char is an operator (not a number)
+            ops_in_a_row += 1
+            if i == 0:
+                if c == "-":
+                    neg_pos.append(i)
+                    continue
+                else:
+                    return "Cannot have '+' or '*' operator at beginning of expression"
+            if not eq[i-1].isalnum():
+                # if the current and prev chars are operators, then this one must be '-' and should be skipped
+                if ops_in_a_row == 3:
+                    return "Cannot have more than 2 operators in a row"
+                neg_pos.append(i)
+                continue
+            ops.append(c)
+        else:
+            ops_in_a_row = 0
+
+    nums = eq.replace("+", ",").replace("-", ",").replace("*", ",").replace("/", ",").replace("^", ",")
+    # [nums.remove(c) for c in nums if c == '']
+    for i in range(len(nums)):
+        if i in neg_pos:
+            nums = nums[:i] + "-" + nums[i+1:]
+
+    nums = nums.split(",")
+    return nums
+
+print(calc("1/2/3"))
