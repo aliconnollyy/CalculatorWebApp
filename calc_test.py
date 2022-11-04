e79d71c1ca103ba73b4a3f0e6c87256e910e7ad4
okaforc <okaforc@tcd.ie>
2022-11-04 15:17:02 +0000

    fixed issues with tests

--- calc_test.py
+++ calc_test.py
@@ -1,9 +1,5 @@
 from calculator import *
 
-# Testing add_in_string method
-def test_add_in_string():
-
-
 # Testing calc method
 def test_calc():
     ts_1 = "1+2"
@@ -15,17 +11,24 @@ def test_calc():
     ts_7 = "-1-2*-3+-1000*0+3"
     ts_8 = "3*3*3*3*3*3*-3*-3*-3"
     ts_9 = "1/2/3"
-    ts_10 = "5^7^39384"
-    assert calc(ts_1) == ["1", "2"]
-    assert calc(ts_2) == ["0", "0"]
-    assert calc(ts_3) == ["100", "100", "1001"]
-    assert calc(ts_4) == ["3", "3", "1"]
-    assert calc(ts_5) == ["0", "4", "0", "180", "1"]
-    assert calc(ts_6) == ["8", "2", "4", "4"]
-    assert calc(ts_7) == ['-1', '2', '-3', '-1000', '0', '3']
-    assert calc(ts_8) == ['3', '3', '3', '3', '3', '3', '-3', '-3', '-3']
-    assert calc(ts_9) == ['1', '2', '3']
-    assert calc(ts_10) == ['5', '7', '39384']
+    ts_10 = "5^7^3"
+    assert calc(ts_1) == 3
+    assert calc(ts_2) == 0
+    assert calc(ts_3) == -1001
+    assert calc(ts_4) == 8
+    assert calc(ts_5) == 1
+    assert calc(ts_6) == 0
+    assert calc(ts_7) == 8
+    assert calc(ts_8) == -19683
+    assert calc(ts_9) == 0
+    assert calc(ts_10) == 476837158203125
+    
+    assert calc('10/2') == 5
+    assert calc('4*2-3+5') == 10
+    assert calc('2+3-1') == 4
+    assert calc('3^2') == 9
+    assert calc('3+4^2') == 19
+    assert calc('3/0') == "Invalid: Division by zero"
 
 
 
@@ -43,30 +46,19 @@ def test_sub_calc():
 # Testing validate method
 def test_validate():
     # Testing invalid inputs
-    assert(3+4-1.) == "Invalid: Can only have integers"
-    assert(.) == "Invalid: Can only have integers"
-    assert(+3+4^2) == "Invalid: Can only have number or \"-\" at beginning of expression"
-    assert(5*3+) == "Invalid: Cannot end expression with an operator"
-    assert(4&5-2) == "Invalid: Contains invalid character: \"" + eq[i] + "\""
-    assert(3*4+++2) == "Invalid: can't have more than three operators in a row"
-    assert(5+9-2+*)) == "Invalid: can't have more than three operators in a row"
-    assert(8+9**2) == "Invalid: can't have duplicate \"*\" operators in a row"
-    assert(3+++) == "Invalid: can't have more than three \"" + eq[i] + "\" operators in a row"
-    assert(5---) == "Invalid: can't have more than three \"" + eq[i] + "\" operators in a row"
+    assert(validate('3+4-1.')) == "Invalid: Can only have integers"
+    assert(validate('.')) == "Invalid: Can only have integers"
+    assert(validate('+3+4^2')) == "Invalid: Can only have number or \"-\" at beginning of expression"
+    assert(validate('5*3+')) == "Invalid: Cannot end expression with an operator"
+    assert(validate('4&5-2')) == "Invalid: Contains invalid character: \"&\""
+    assert(validate('3*4+++2')) == "Invalid: can't have more than three operators in a row"
+    assert(validate('5+9-2+*)')) == "Invalid: can't have more than three operators in a row"
+    assert(validate('8+9**2')) == "Invalid: can't have duplicate \"*\" operators in a row"
+    assert(validate('3+++3')) == "Invalid: can't have more than three operators in a row"
+    assert(validate('5---98')) == "Invalid: can't have more than three operators in a row"
     # Testing valid inputs
-    assert(4+3*4-1) == "!4+3*4-1"
-    assert(21/3) == "!21/3"
-    assert(2^3) == "!2^3"
-    assert(9++) == "!9++"
-    assert(3--) == "!3--"
-
-
-# Testing sep method
-def test_sep():
-    assert(10/2) == "5"
-    assert(4*2-3+5) == "10"
-    assert((2+3)-1) == "4"
-    assert(3^2) == "9"
-    assert(3+(4^2)) = "19"
-    assert(3/0) == "Invalid: Division by zero"
-    
+    assert(validate('4+3*4-1')) == "!4+3*4-1"
+    assert(validate('21/3')) == "!21/3"
+    assert(validate('2^3')) == "!2^3"
+    assert(validate('9++8')) == "!9++8"
+    assert(validate('3--1')) == "!3--1"
